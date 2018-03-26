55c859c9dd2c3aa6f3bbfdc45afe318473469807
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-03-26 20:51:04 +0200

    generating permutation

--- Ormat/ORMAT.pl
+++ Ormat/ORMAT.pl
@@ -8,9 +8,41 @@ generuj([H|T], N, [H|L]) :- !,
 	N1 is N-1, 
 	generuj(T, N1, L).
 
+pobierz(X,[X|R],R).  
+pobierz(X,[F|R],[F|S]) :-
+	pobierz(X,R,S).
+
+perm([X|Y],Z) :- 
+	perm(Y,W), 
+	pobierz(X,Z,W).  
+perm([],[]).
+
+silnia(0,1).
+silnia(N,X):-
+	N1 is N-1,
+	silnia(N1,X1),
+	X is N*X1.
+
+
+isList([_|_]).
+isList([]).
 
 start:-
-	generuj([],3, W),
+	V is 4,
+	% generowanie listy od 1 do V i jej wyświetlenie
+ 	generuj([],V, W),	
 	writeln(W),
+
+	% znalezienie permutacji listy W i ich wyświetlenie
+	perm(W, X),
+	writeln(X),	
+
+	% ilość permutacji obliczona 
+	silnia(V, Y),
+	write('Ilosc permutacji:'),	
+	writeln(Y),
+
+	% ilość permutacji -> długość listy X
+	write('Długosc listy:'),	
 	fail.
 start.
