4d1ae926a051eac9ea034ca25adc0c3da3f49e23
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-03-26 20:21:55 +0200

    generating list - don't touch!!

--- Ormat/ORMAT.pl
+++ Ormat/ORMAT.pl
@@ -1,40 +1,16 @@
-:- use_module(library(clpfd)).
 
-sudoku(Rows) :-
-		length(Rows, 9), 
-		maplist(same_length(Rows), Rows),
+ 
+generuj([], 0, []) :- !.
+generuj([], N, [N|L]) :- !, 
+	N1 is N-1, 
+	generuj([], N1, L).
+generuj([H|T], N, [H|L]) :- !,
+	N1 is N-1, 
+	generuj(T, N1, L).
 
-		append(Rows, Vs), 	
-		Vs ins 1..9,
-
-        maplist(all_distinct, Rows),
-        transpose(Rows, Columns),
-		maplist(all_distinct, Columns),
-		
-        Rows = [As,Bs,Cs,Ds,Es,Fs,Gs,Hs,Is],
-        blocks(As, Bs, Cs),
-        blocks(Ds, Es, Fs),
-        blocks(Gs, Hs, Is).
-
-blocks([], [], []).
-blocks([N1,N2,N3|Ns1], [N4,N5,N6|Ns2], [N7,N8,N9|Ns3]) :-
-		writeln(Ns3),
-        all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
-        blocks(Ns1, Ns2, Ns3).
-
-problem(1, [[_,_,_,_,_,_,_,_,_],
-            [_,_,_,_,_,3,_,8,5],
-            [_,_,1,_,2,_,_,_,_],
-            [_,_,_,5,_,7,_,_,_],
-            [_,_,4,_,_,_,1,_,_],
-            [_,9,_,_,_,_,_,_,_],
-            [5,_,_,_,_,_,_,7,3],
-            [_,_,2,_,1,_,_,_,_],
-            [_,_,_,_,4,_,_,_,9]]).
 
 start:-
-	problem(1, Rows),
-	sudoku(Rows),
-	maplist(portray_clause, Rows),
+	generuj([],3, W),
+	writeln(W),
 	fail.
 start.
