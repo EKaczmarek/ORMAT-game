ec247c386271c8e1333c474e33848a1cc28d1f23
DESKTOP-DMGLT16\Ela <e.kaczmarek01@gmail.com>
2018-04-25 10:28:23 +0200

    Matrixes

--- Ormat/permutations_CLP.pl
+++ Ormat/permutations_CLP.pl
@@ -1,12 +1,20 @@
 :- use_module(library(clpfd)).
 
+getPatterns(Lst, Qs):-
+	/*Lst np[1,1,1, 1,1,1, 1,1,0 ]*/
+	length(Lst, W),
+	D is integer(sqrt(W)),
+	permutation_clp(D, Qs).
+
 permutation_clp(N, Qs) :-
 	length(Qs, N),
 	Qs ins 1..N,
 	find_p(Qs).
 
 find_p([]).
-find_p([Q|Qs]) :- find_p(Qs, Q, 1), find_p(Qs).
+find_p([Q|Qs]) :- 
+	find_p(Qs, Q, 1), 
+	find_p(Qs).
 
 find_p([], _, _).
 find_p([Q|Qs], Q0, D0) :-
@@ -16,3 +24,5 @@ find_p([Q|Qs], Q0, D0) :-
 	
 	
 /*Wywołanie ?-n_queens(4, Qs), label(Qs).*/
+
+
